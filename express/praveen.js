6c9905a632ad485701c3cae9c0d61d8eeec5bd1e
PrasathRvg <prasathrvg@gmail.com>
2021-09-06 18:32:50 +0530

    staticfiles

--- express/praveen.js
+++ express/praveen.js
@@ -9,6 +9,13 @@ app.set('views','./views');
 app.use(express.urlencoded({extened:true}));
 app.use(express.json());
 
+app.use(express.static(__dirname+"/open"))  
+
+app.use(function(req, res, next){
+    console.log("middle ware executed")
+    next()
+})
+
 app.get("/",function(req,res){
     res.sendFile(__dirname+"/homepage.html");
 })  
@@ -16,11 +23,7 @@ app.get("/",function(req,res){
 app.use("/student",studentRoutes)
 app.use("/employee",employeeRoutes) 
 
-// router.get("/studentlist/fulldetails",function(req,res){
-//     res.render("post",{
-//         allstudents:students
-//     })
-// })
+
 
 app.listen(7007,function(req,res){
     console.log("listening on 7007")
