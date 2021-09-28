1adda232623a71920029b7416f7e6efb176b8368
PrasathRvg <prasathrvg@gmail.com>
2021-09-28 20:26:39 +0530

    middleware

--- contacts/index.js
+++ contacts/index.js
@@ -1,10 +1,12 @@
 var express=require("express");
 var app=express();
 var {MongoClient,ObjectId} = require('mongodb');
+var cors = require('cors');
 var multer=require("multer");
 var path=require("path");
 
 app.use(express.static("uploads"))  
+app.use(cors());
 
 const storage=multer.diskStorage({destination:function(req,file,cb){
     cb(null,__dirname+"/uploads")
@@ -27,7 +29,14 @@ app.set('views','./views');
 app.use(express.urlencoded({extended: true})); 
 app.use(express.json());  
 
+//homepage
 app.get("/",function(req,res){
+    res.render("homepage.pug")
+})
+
+//contactlist
+
+app.get("/contactlist",function(req,res){
     MongoClient.connect(url,function(err,conn){
         var db=conn.db("delta");
         db.collection("contacts").find().toArray(function(err,data){
@@ -38,6 +47,20 @@ app.get("/",function(req,res){
     })
 })
 
+///contactlist for postman
+
+// app.get("/list",function(req,res){
+//     MongoClient.connect(url,function(err,conn){
+//         var db=conn.db("delta");
+//         db.collection("contacts").find().toArray(function(err,data){
+//             res.send(data);
+//         })
+//     })
+// })
+
+
+//add contact form
+
 app.get("/addcontact",function(req,res){
     res.render("addcontact")
 })
@@ -47,23 +70,28 @@ app.post("/addcontact",upload.single('profilepic'),function(req,res){
     MongoClient.connect(url,function(err,conn){
         var db=conn.db("delta");
         db.collection("contacts").insertOne(req.body,function(err,data){
-            // res.send("registered successfully")
-            res.redirect("/")
+            res.send("registered successfully")
+            // res.redirect("/contactlist")
         })
     })
 })
+
+
+//delete contact
 
 app.get("/deletecontact/:id",function(req,res){
     MongoClient.connect(url,function(err,conn){
         var db=conn.db("delta")
         db.collection("contacts").deleteOne({_id:ObjectId(req.params.id)},function(err,data){
-            res.redirect("/")
+            // res.redirect("/contactlist")
+            res.send("deleted successfully");
         })
     })    
 })
 
+//update contact
+
 app.get("/editcontact/:id",function(req,res){
-    console.log("req.params::",req.params)
     MongoClient.connect(url,function(err,conn){
         var db=conn.db("delta");
         db.collection("contacts").findOne({_id:ObjectId(req.params.id)},function(err,data){
@@ -82,16 +110,58 @@ app.post("/editcontact",upload.single('profilepic'),function(req,res){
             $set:{
                     name:req.body.name,
                     mobile:req.body.mobile,
+                    group:req.body.group,
                     profilepic:req.file.filename
                 }
         },function(err,data){
             console.log(data)
-            res.redirect("/")
+            res.redirect("/contactlist")
             // res.send("edited successfully")
         })
     })
 })
 
+//middleware
+
+function contacts(req,res,next){
+    var group=req.url.replace("/","");
+    // console.log("group::",group)
+    MongoClient.connect(url,function(err,conn){
+        var db=conn.db("delta");
+        db.collection("contacts").find({group:group}).toArray(function(err,data){
+            details=data
+            next()
+        })
+    })
+}
+
+app.get('/friend',contacts,function(req,res){
+    // console.log("req.url::",req.url);
+    res.render("contactlist.pug",{
+        details:details
+    })
+})
+
+app.get('/family',contacts,function(req,res){
+    // console.log("req.url::",req.url);
+    res.render("contactlist.pug",{
+        details:details
+    })
+})
+
+app.get('/work',contacts,function(req,res){
+    // console.log("req.url::",req.url);
+    res.render("contactlist.pug",{
+        details:details
+    })
+})
+
+app.get('/relative',contacts,function(req,res){
+    // console.log("req.url::",req.url);
+    res.render("contactlist.pug",{
+        details:details
+    })
+})
 
 app.listen(9999,function(req,res){
     console.log("Running on 9999")
